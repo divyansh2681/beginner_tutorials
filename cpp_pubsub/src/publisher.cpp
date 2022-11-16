b735f22000f2478d7c54787e594436d8b17fba23
divyansh2681 <divyanshagarwal81@gmail.com>
2022-11-15 20:17:34 -0500

    updated publisher.cpp

--- cpp_pubsub/src/publisher.cpp
+++ cpp_pubsub/src/publisher.cpp
@@ -17,14 +17,13 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher()
   : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      std::chrono::milliseconds(500), std::bind(&MinimalPublisher::timer_callback, this));
   }
 
  private:
