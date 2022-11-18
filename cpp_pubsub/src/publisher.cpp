7f4efea91258386ac836dbb160e13d183ec87b7a
divyansh2681 <divyanshagarwal81@gmail.com>
2022-11-17 21:56:51 -0500

    updated the publisher, subscriber and client files

--- cpp_pubsub/src/publisher.cpp
+++ cpp_pubsub/src/publisher.cpp
@@ -11,97 +11,67 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#include <string>
 #include <chrono>
 #include <functional>
 #include <memory>
-#include <rcl_interfaces/msg/detail/parameter_descriptor__struct.hpp>
+#include <string>
 
-
-#include "cpp_pubsub/srv/count.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "cpp_pubsub/srv/stringss.hpp"
 
 using namespace std::chrono_literals;
+using Stringss = cpp_pubsub::srv::Stringss;
 using namespace std::placeholders;
-using Count = cpp_pubsub::srv::Count;
 
-
-class MinimalPublisher : public rclcpp::Node{
+class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0), ctr_(1000) {
+  : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      5000ms, std::bind(&MinimalPublisher::timer_callback, this));
 
-    if (rcutils_logging_set_logger_level(this->get_logger().get_name(),
-                      RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG)
-          == RCUTILS_RET_OK) {
-          RCLCPP_INFO_STREAM(this->get_logger(),
-                             "Set logger level DEBUG success.");
-      } else {
-          RCLCPP_ERROR_STREAM(this->get_logger(),
-                              "Set logger level DEBUG fails.");
-      }
-      this->declare_parameter("count", ctr_);
-
-    std::string get_count_service_name =
-        "/" + std::string(this->get_name()) + "/" + "Count";
-    get_count_service_ = this->create_service<Count>(
-        get_count_service_name,
-        std::bind(&MinimalPublisher::get_count_callback, this, _1, _2));
+    service_ = this->create_service<Stringss>("service_to_minimal_publisher", \
+    std::bind(&MinimalPublisher::RespondToClient, this, _1, _2));
   }
 
  private:
   void timer_callback() {
-    ctr_ = this->get_parameter("count").get_parameter_value().get<int>();
-    if (count_ < ctr_) {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, this publisher is written by Divyansh "
-                    + std::to_string(count_++);
-    switch ((count_-1)%5) {
-    case 0:
-      RCLCPP_INFO_STREAM(this->get_logger(),
-                          "Publishing:"<< message.data.c_str());
-      break;
-
-    case 1:
-      RCLCPP_DEBUG_STREAM(this->get_logger(),
-                           "Publishing:"<< message.data.c_str());
-      break;
-
-    case 2:
-      RCLCPP_WARN_STREAM(this->get_logger(),
-                           "Publishing:"<< message.data.c_str());
-      break;
-
-    case 3:
-      RCLCPP_ERROR_STREAM(this->get_logger(),
-                           "Publishing:"<< message.data.c_str());
-      break;
-
-    case 4:
-      RCLCPP_FATAL_STREAM(this->get_logger(),
-                           "Publishing:"<< message.data.c_str());
-      break;
-    }
+    message.data = "Hello, this is the publisher for ENPM808X by Divyansh " \
+    + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+  }
+
+  void RespondToClient(const std::shared_ptr<Stringss::Request> request,
+          std::shared_ptr<Stringss::Response> response) {
+    response->output = request->input;
+
+    RCLCPP_INFO(this->get_logger(), "Incoming request\na: [%s]",
+                  request->input.c_str());
+
+    std::string varr = std::string(response->output.c_str());
+    if (varr == "DEBUG") {
+      RCLCPP_DEBUG(this->get_logger(), response->output.c_str());
+    } else if (varr == "WARN") {
+      RCLCPP_WARN(this->get_logger(), response->output.c_str());
+    } else if (varr == "FATAL") {
+      RCLCPP_FATAL(this->get_logger(), response->output.c_str());
+    } else if (varr == "ERROR") {
+      RCLCPP_ERROR(this->get_logger(), response->output.c_str());
+    } else {
+      RCLCPP_INFO(this->get_logger(), response->output.c_str());
     }
-  }
 
-  void get_count_callback(const std::shared_ptr<Count::Request> request,
-                          std::shared_ptr<Count::Response> response) {
-    (void)request;
-    response->count = ctr_;
+    RCLCPP_INFO(this->get_logger(), "sending back response: [%s]", \
+    response->output.c_str());
   }
-
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  rclcpp::Service<Count>::SharedPtr get_count_service_;
-  int count_;
-  int ctr_;
+  rclcpp::Service<Stringss>::SharedPtr service_;
+  size_t count_;
 };
 
 int main(int argc, char * argv[]) {
